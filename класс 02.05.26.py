b07c39f07cd8d0ae0c2333fb5b6b33b1e6e11770
Polina <gs0941915@gmail.com>
2026-05-02 15:40:34 +0300

    add

--- класс 02.05.26.py
+++ класс 02.05.26.py
@@ -11,10 +11,20 @@ def decorator(func):
 
     return wrapper
 
-@decorator
+
 def say_Hello():
     print("Hello")
 
 
 
 say_Hello()
+
+proSayHello = decorator(say_Hello)
+proSayHello()
+
+def decorator(func):
+    def wrapper(*args, **kwargs):
+        print("Calling decorated function")
+        result = func(*args, **kwargs)
+        return result
+    return wrapper
